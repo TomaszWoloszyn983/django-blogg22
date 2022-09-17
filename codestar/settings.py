458ae3d15715b0ef795fdcdb42c269eeaa303151
Tomasz <tomaszwoloszyn983@gmail.com>
2022-09-17 11:35:58 +0000

    Fix deployment error

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
-    'blog',
+    'django.contrib.staticfiles',
     'cloudinary',
+    'blog',
 ]
 
 MIDDLEWARE = [
